692deff3af4912221496214c1654386b70597fc8
Paul Stryck <6186300+PaulStryck@users.noreply.github.com>
2022-08-24 12:09:19 +0200

    remove arduino dependency in favor of asf4

--- src/LXSAMD21DMX.cpp
+++ src/LXSAMD21DMX.cpp
@@ -14,45 +14,41 @@
 /**************************************************************************/
 
 #include "LXSAMD21DMX.h"
-#include "Arduino.h"
-#include "wiring_private.h"
-#include "variant.h"
-#include <inttypes.h>
-#include <stdlib.h>
+#include <compiler.h>
+
+// #include <hal_gpio.h>
+// #include <inttypes.h>
+// #include <stdlib.h>
+#include <cstring>
+#include <hpl_usart.h>
+#include <peripheral_clk_config.h>
 
 //**************************************************************************************
 
-LXSAMD21DMX SAMD21DMX;
+// TODO: make basic usart config a parameter to class
 
-// **************************** SERCOMn_Handler  ***************
-
-void SERCOM4_Handler()
-{
-  SAMD21DMX.IrqHandler();
-}
 
 
 // ***************** define registers, flags and interrupts  ****************
 
-	//***** baud rate defines
-    #define DMX_DATA_BAUD		250000
-    #define DMX_BREAK_BAUD 	90000
+    //***** baud rate defines
+    #define DMX_DATA_BAUD   250000
+    #define DMX_BREAK_BAUD  250000
     //99900
 
     //***** states indicate current position in DMX stream
     #define DMX_STATE_BREAK 0
     #define DMX_STATE_START 1
-    #define DMX_STATE_DATA 2
-    #define DMX_STATE_IDLE 3
+    #define DMX_STATE_DATA  2
+    #define DMX_STATE_IDLE  3
 
-	//***** status is if interrupts are enabled and IO is active
-    #define ISR_DISABLED 			0
-    #define ISR_OUTPUT_ENABLED 	1
-    #define ISR_INPUT_ENABLED 	2
+    //***** status is if interrupts are enabled and IO is active
+    #define ISR_DISABLED        0
+    #define ISR_OUTPUT_ENABLED  1
+    #define ISR_INPUT_ENABLED   2
 
 // **************************** global data (can be accessed in ISR)  ***************
 
-Uart SerialDMX (&DMX_sercom, PIN_DMX_RX, PIN_DMX_TX, PAD_DMX_RX, PAD_DMX_TX);
 
 uint8_t*  _shared_dmx_data;
 uint8_t   _shared_dmx_state;
@@ -61,31 +57,72 @@ uint16_t  _shared_max_slots = DMX_MIN_SLOTS;
 LXRecvCallback _shared_receive_callback = NULL;
 
 
-void setBaudRate(uint32_t baudrate) {
-	DMX_SERCOM->USART.CTRLA.bit.ENABLE = 0x0u; // must be disabled before writing to USART.BAUD or USART.CTRLA
-	uint16_t sampleRateValue = 16;
+static void init_dmx_uart(void *const hw, dmx_usart_configuration conf);
 
-    // see SERCOM.initUART
-    uint32_t baudTimes8 = (SystemCoreClock * 8) / (sampleRateValue * baudrate);
 
-    DMX_SERCOM->USART.BAUD.FRAC.FP   = (baudTimes8 % 8);
-    DMX_SERCOM->USART.BAUD.FRAC.BAUD = (baudTimes8 / 8);
-    DMX_SERCOM->USART.CTRLA.bit.ENABLE = 0x1u; // re-enable
+// USART_0_CLOCK_init() before this
+static void init_dmx_uart(void *const hw, dmx_usart_configuration conf) {
+    ASSERT(hw);
+
+    // usart_init
+    if (!hri_sercomusart_is_syncing(hw, SERCOM_USART_SYNCBUSY_SWRST)) {
+        uint32_t mode = conf.ctrl_a & SERCOM_USART_CTRLA_MODE_Msk;
+        if (hri_sercomusart_get_CTRLA_reg(hw, SERCOM_USART_CTRLA_ENABLE)) {
+            hri_sercomusart_clear_CTRLA_ENABLE_bit(hw);
+            hri_sercomusart_wait_for_sync(hw, SERCOM_USART_SYNCBUSY_ENABLE);
+        }
+        hri_sercomusart_write_CTRLA_reg(hw, SERCOM_USART_CTRLA_SWRST | mode);
+    }
+    hri_sercomusart_wait_for_sync(hw, SERCOM_USART_SYNCBUSY_SWRST);
+
+    hri_sercomusart_write_CTRLA_reg(hw, conf.ctrl_a);
+    hri_sercomusart_write_CTRLB_reg(hw, conf.ctrl_b);
+    if ((conf.ctrl_a & SERCOM_USART_CTRLA_SAMPR(0x1)) || (conf.ctrl_a & SERCOM_USART_CTRLA_SAMPR(0x3))) {
+        ((Sercom *)hw)->USART.BAUD.FRAC.BAUD = conf.baud;
+        ((Sercom *)hw)->USART.BAUD.FRAC.FP   = conf.fractional;
+    } else {
+        hri_sercomusart_write_BAUD_reg(hw, conf.baud);
+    }
+
+    hri_sercomusart_write_RXPL_reg(hw, conf.rxpl);
+    hri_sercomusart_write_DBGCTRL_reg(hw, conf.debug_ctrl);
+
+    // Async NVIC init
+    IRQn_Type IdNvic = SERCOM0_IRQn; // TODO!
+    NVIC_DisableIRQ(IdNvic);
+    NVIC_ClearPendingIRQ(IdNvic);
+    NVIC_EnableIRQ(IdNvic);
+    // NVIC_SetPriority (IdNvic, SERCOM_NVIC_PRIORITY);  /* set Priority */
+
+    // Set CTRLA register
+    // Set INTENSET register
+    hri_sercomusart_write_INTEN_RXC_bit(hw, true);
+    hri_sercomusart_write_INTEN_ERROR_bit(hw, true);
+
+    // Mode is Internal clock
+    // compute baud rate and set
+
+    // enable
+    hri_sercomusart_set_CTRLA_ENABLE_bit(hw);
+    // hri_sercomusart_clear_CTRLA_ENABLE_bit(hw);
+    hri_sercomusart_wait_for_sync(hw, SERCOM_USART_SYNCBUSY_ENABLE);
 }
+
 
 //************************************************************************************
 // ************************  LXSAMD21DMXOutput member functions  ********************
 
-LXSAMD21DMX::LXSAMD21DMX ( void ) {
-	_direction_pin = DIRECTION_PIN_NOT_USED;	//optional
-	_shared_max_slots = DMX_MAX_SLOTS;
-	_interrupt_status = ISR_DISABLED;
-	
-	//zero buffer including _dmxData[0] which is start code
+LXSAMD21DMX::LXSAMD21DMX ( Sercom *const hw, dmx_usart_configuration conf )
+    : _hw(hw), _conf(conf) {
+    _direction_pin = DIRECTION_PIN_NOT_USED;    //optional
+    _shared_max_slots = DMX_MAX_SLOTS;
+    _interrupt_status = ISR_DISABLED;
+
+    //zero buffer including _dmxData[0] which is start code
     memset(_dmxData, 0, DMX_MAX_SLOTS+1);
 }
-    
-    
+
+
 
 LXSAMD21DMX::~LXSAMD21DMX ( void ) {
     stop();
@@ -94,87 +131,116 @@ LXSAMD21DMX::~LXSAMD21DMX ( void ) {
 }
 
 
+void LXSAMD21DMX::setBaudRate(uint32_t baudrate) {
+    ASSERT(_hw);
+
+    hri_sercomusart_clear_CTRLA_ENABLE_bit(_hw);
+    hri_sercomusart_wait_for_sync(_hw, SERCOM_USART_SYNCBUSY_ENABLE);
+
+    // TODO: get actual sample rate
+    uint16_t sampleRateValue = 16;
+
+    // see SERCOM.initUART
+    // TODO: get actual clock speed
+    uint32_t baudTimes8 = (4000000 * 8) / (sampleRateValue * baudrate);
+
+    _hw->USART.BAUD.FRAC.FP   = (baudTimes8 % 8);
+    _hw->USART.BAUD.FRAC.BAUD = (baudTimes8 / 8);
+
+    hri_sercomusart_set_CTRLA_ENABLE_bit(_hw);
+    hri_sercomusart_wait_for_sync(_hw, SERCOM_USART_SYNCBUSY_ENABLE);
+}
+
 void LXSAMD21DMX::startOutput ( void ) {
-	if ( _direction_pin != DIRECTION_PIN_NOT_USED ) {
-		digitalWrite(_direction_pin, HIGH);
-	}
+    if ( _direction_pin != DIRECTION_PIN_NOT_USED ) {
+        gpio_set_pin_level(_direction_pin, true);
+    }
 
-	if ( _interrupt_status == ISR_INPUT_ENABLED ) {
-		stop();
-	}
-	
-	if ( _interrupt_status == ISR_DISABLED ) {	//prevent messing up sequence if already started...
-	  SerialDMX.begin(DMX_BREAK_BAUD, (uint8_t)SERIAL_8N2);
-  
-	  // Assign pin mux to SERCOM functionality (must come after SerialDMX.begin)
-	  pinPeripheral(PIN_DMX_RX, MUX_DMX_RX);
-	  pinPeripheral(PIN_DMX_TX, MUX_DMX_TX);
+    if ( _interrupt_status == ISR_INPUT_ENABLED ) {
+        stop();
+    }
 
-	  _interrupt_status = ISR_OUTPUT_ENABLED;
-	  _shared_dmx_data = dmxData();
-	  _shared_dmx_slot = 0;              
-	  _shared_dmx_state = DMX_STATE_START;
+    if ( _interrupt_status == ISR_DISABLED ) {  //prevent messing up sequence if already started...
+                                                //SerialDMX.begin(DMX_BREAK_BAUD, (uint8_t)SERIAL_8N2);
+        init_dmx_uart(_hw, _conf);
+        setBaudRate(DMX_BREAK_BAUD);
 
-	  DMX_SERCOM->USART.INTENSET.reg =  SERCOM_USART_INTENSET_TXC | SERCOM_USART_INTENSET_ERROR;
-	  DMX_SERCOM->USART.DATA.reg = 0;
+        // Assign pin mux to SERCOM functionality (must come after SerialDMX.begin)
+        gpio_set_pin_function(PIN_DMX_RX, MUX_DMX_RX);
+        gpio_set_pin_function(PIN_DMX_TX, MUX_DMX_TX);
 
-	}
+        _interrupt_status = ISR_OUTPUT_ENABLED;
+        _shared_dmx_data = dmxData();
+        _shared_dmx_slot = 0;
+        _shared_dmx_state = DMX_STATE_START;
+
+        // _hw->USART.INTENSET.reg =  SERCOM_USART_INTENSET_TXC | SERCOM_USART_INTENSET_ERROR;
+        // _hw->USART.DATA.reg = 0;
+        hri_sercomusart_write_INTEN_RXC_bit(_hw, false);
+        hri_sercomusart_write_INTEN_TXC_bit(_hw, true);
+        hri_sercomusart_write_INTEN_ERROR_bit(_hw, true);
+
+        hri_sercomusart_write_DATA_reg(_hw, 1);
+
+    }
 }
 
 void LXSAMD21DMX::startInput ( void ) {
-	if ( _direction_pin != DIRECTION_PIN_NOT_USED ) {
-		digitalWrite(_direction_pin, LOW);
-	}
-	if ( _interrupt_status == ISR_OUTPUT_ENABLED ) {
-		stop();
-	}
-	if ( _interrupt_status == ISR_DISABLED ) {	//prevent messing up sequence if already started...
-		SerialDMX.begin(DMX_DATA_BAUD, (uint8_t)SERIAL_8N2);
-  
-	   // Assign pin mux to SERCOM functionality (must come after SerialDMX.begin)
-	   pinPeripheral(PIN_DMX_RX, MUX_DMX_RX);
-	   pinPeripheral(PIN_DMX_TX, MUX_DMX_TX);
+    if ( _direction_pin != DIRECTION_PIN_NOT_USED ) {
+        gpio_set_pin_level(_direction_pin, false);
+    }
+    if ( _interrupt_status == ISR_OUTPUT_ENABLED ) {
+        stop();
+    }
+    if ( _interrupt_status == ISR_DISABLED ) {  //prevent messing up sequence if already started...
+        //SerialDMX.begin(DMX_DATA_BAUD, (uint8_t)SERIAL_8N2);
+        init_dmx_uart(_hw, _conf);
+        setBaudRate(DMX_DATA_BAUD);
 
-		_shared_dmx_data = dmxData();
-		_shared_dmx_slot = 0;              
-		_shared_dmx_state = DMX_STATE_IDLE;
+        // Assign pin mux to SERCOM functionality (must come after SerialDMX.begin)
+        gpio_set_pin_function(PIN_DMX_RX, MUX_DMX_RX);
+        gpio_set_pin_function(PIN_DMX_TX, MUX_DMX_TX);
 
-		_interrupt_status = ISR_INPUT_ENABLED;
-	}
+        _shared_dmx_data = dmxData();
+        _shared_dmx_slot = 0;
+        _shared_dmx_state = DMX_STATE_IDLE;
+
+        _interrupt_status = ISR_INPUT_ENABLED;
+    }
 }
 
 void LXSAMD21DMX::stop ( void ) {
-   SerialDMX.end();
-	_interrupt_status = ISR_DISABLED;
+   // SerialDMX.end();
+    _interrupt_status = ISR_DISABLED;
 }
 
 void LXSAMD21DMX::setDirectionPin( uint8_t pin ) {
-	_direction_pin = pin;
-	pinMode(_direction_pin, OUTPUT);
+    _direction_pin = pin;
+    gpio_set_pin_direction(_direction_pin, GPIO_DIRECTION_OUT);
 }
 
 void LXSAMD21DMX::setMaxSlots (int slots) {
-	if ( slots > DMX_MIN_SLOTS ) {
-		_shared_max_slots = slots;
-	} else {
-		_shared_max_slots = DMX_MIN_SLOTS;
-	}
+    if ( slots > DMX_MIN_SLOTS ) {
+        _shared_max_slots = slots;
+    } else {
+        _shared_max_slots = DMX_MIN_SLOTS;
+    }
 }
 
 uint8_t LXSAMD21DMX::getSlot (int slot) {
-	return _dmxData[slot];
+    return _dmxData[slot];
 }
 
 void LXSAMD21DMX::setSlot (int slot, uint8_t value) {
-	_dmxData[slot] = value;
+    _dmxData[slot] = value;
 }
 
 uint8_t* LXSAMD21DMX::dmxData(void) {
-	return &_dmxData[0];
+    return &_dmxData[0];
 }
 
 void LXSAMD21DMX::setDataReceivedCallback(LXRecvCallback callback) {
-	_shared_receive_callback = callback;
+    _shared_receive_callback = callback;
 }
 
 
@@ -182,11 +248,11 @@ void LXSAMD21DMX::IrqHandler(void) {
 
   if ( _interrupt_status == ISR_OUTPUT_ENABLED ) {
 
-    if ( DMX_SERCOM->USART.INTFLAG.bit.TXC ) {
-      if ( DMX_SERCOM->USART.INTFLAG.bit.DRE ) {
+    if ( _hw->USART.INTFLAG.bit.TXC ) {
+      if ( _hw->USART.INTFLAG.bit.DRE ) {
 
         if ( _shared_dmx_state == DMX_STATE_DATA ) {
-          DMX_SERCOM->USART.DATA.reg = _shared_dmx_data[_shared_dmx_slot++];	//send next slot;
+          hri_sercomusart_write_DATA_reg(_hw,_shared_dmx_data[_shared_dmx_slot++]);    //send next slot;
           if ( _shared_dmx_slot > _shared_max_slots ) {
             _shared_dmx_state = DMX_STATE_BREAK;
           }
@@ -194,56 +260,56 @@ void LXSAMD21DMX::IrqHandler(void) {
           setBaudRate(DMX_BREAK_BAUD);
           _shared_dmx_state = DMX_STATE_START;
           _shared_dmx_slot = 0;
-          DMX_SERCOM->USART.DATA.reg = 0;	//break
+          hri_sercomusart_write_DATA_reg(_hw, 0); // break
         } else if ( _shared_dmx_state == DMX_STATE_START ) {
           setBaudRate(DMX_DATA_BAUD);
           _shared_dmx_state = DMX_STATE_DATA;
-          DMX_SERCOM->USART.DATA.reg = _shared_dmx_data[_shared_dmx_slot++];
+          hri_sercomusart_write_DATA_reg(_hw, _shared_dmx_data[_shared_dmx_slot++]);
         }
-      
+
       }  //DRE
     }    //TXC
-    
+
   } else if ( _interrupt_status == ISR_INPUT_ENABLED ) {
-	
-		if ( DMX_SERCOM->USART.INTFLAG.bit.ERROR ) {
-		   DMX_SERCOM->USART.INTFLAG.bit.ERROR = 1;		//acknowledge error, clear interrupt
-		   
-			if ( DMX_SERCOM->USART.STATUS.bit.FERR ) {	//framing error happens when break is sent
-				_shared_dmx_state = DMX_STATE_BREAK;
-				if ( _shared_dmx_slot > 0 ) {
-					if ( _shared_receive_callback != NULL ) {
-						_shared_receive_callback(_shared_dmx_slot);
-					}
-				}
-				_shared_dmx_slot = 0;
-			  return;
-			}
-			// other error flags?
-			//return;?
-		}	//ERR
-	
-		if ( DMX_SERCOM->USART.INTFLAG.bit.RXC ) {
-		uint8_t incoming_byte = DMX_SERCOM->USART.DATA.reg;				// read buffer to clear interrupt flag
-			switch ( _shared_dmx_state ) {
-				case DMX_STATE_BREAK:
-					if ( incoming_byte == 0 ) {									// start code == zero (DMX)
-						_shared_dmx_data[_shared_dmx_slot] = incoming_byte;
-						_shared_dmx_state = DMX_STATE_DATA;
-					} else {
-						_shared_dmx_state = DMX_STATE_IDLE;
-					}
-					break;
-			
-				case DMX_STATE_DATA:
-					_shared_dmx_data[_shared_dmx_slot++] = incoming_byte;	// increments BEFORE assignment
-					if ( _shared_dmx_slot > DMX_MAX_SLOTS ) {
-						_shared_dmx_state = DMX_STATE_IDLE;						// go to idle, wait for next break
-					}
-					break;
-			}
-		} // RXC
-    
-  }	  // input enabled
-	
-}		  //--IrqHandler(void)
+
+        if ( _hw->USART.INTFLAG.bit.ERROR ) {
+           _hw->USART.INTFLAG.bit.ERROR = 1;     //acknowledge error, clear interrupt
+
+            if ( _hw->USART.STATUS.bit.FERR ) {  //framing error happens when break is sent
+                _shared_dmx_state = DMX_STATE_BREAK;
+                if ( _shared_dmx_slot > 0 ) {
+                    if ( _shared_receive_callback != NULL ) {
+                        _shared_receive_callback(_shared_dmx_slot);
+                    }
+                }
+                _shared_dmx_slot = 0;
+              return;
+            }
+            // other error flags?
+            //return;?
+        }   //ERR
+
+        if ( _hw->USART.INTFLAG.bit.RXC ) {
+        uint8_t incoming_byte = _hw->USART.DATA.reg;             // read buffer to clear interrupt flag
+            switch ( _shared_dmx_state ) {
+                case DMX_STATE_BREAK:
+                    if ( incoming_byte == 0 ) {                                 // start code == zero (DMX)
+                        _shared_dmx_data[_shared_dmx_slot] = incoming_byte;
+                        _shared_dmx_state = DMX_STATE_DATA;
+                    } else {
+                        _shared_dmx_state = DMX_STATE_IDLE;
+                    }
+                    break;
+
+                case DMX_STATE_DATA:
+                    _shared_dmx_data[_shared_dmx_slot++] = incoming_byte;   // increments BEFORE assignment
+                    if ( _shared_dmx_slot > DMX_MAX_SLOTS ) {
+                        _shared_dmx_state = DMX_STATE_IDLE;                     // go to idle, wait for next break
+                    }
+                    break;
+            }
+        } // RXC
+
+  }   // input enabled
+
+}         //--IrqHandler(void)
--- src/LXSAMD21DMX.h
+++ src/LXSAMD21DMX.h
@@ -30,7 +30,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
    The LXSAMD21DMX library supports output and input of DMX using
    sercom4 of a MKR1000 microcontroller in UART mode. (pins 7 & 8)
-   
+
    This is the circuit for a simple unisolated DMX Shield
    that could be used with LXSAMD21DMX.  It uses a line driver IC
    to convert the output from the SAM D-21 to DMX:
@@ -56,181 +56,238 @@ TX (4) |----------------------| 4 DI   Gnd 5 |---+------------ Pin 1
 #define LXSAM21_DMX_H
 
 #include <inttypes.h>
-#include "SERCOM.h"
+#include <hpl_usart.h>
+#include <atmel_start_pins.h>
 
 #define DMX_MIN_SLOTS 24
-#define DMX_MAX_SLOTS 512
+#define DMX_MAX_SLOTS 96
 
 #define DIRECTION_PIN_NOT_USED 255
 
+#define DMX_USART_CONF_SAMPLE                                           \
+    {                                                                   \
+        SERCOM_USART_CTRLA_MODE(UART_INT_CLOCK)                         \
+            | (0 << SERCOM_USART_CTRLA_RUNSTDBY_Pos)                    \
+            | (0 << SERCOM_USART_CTRLA_IBON_Pos)                        \
+            | SERCOM_USART_CTRLA_SAMPR(SAMPLE_RATE_x16)                 \
+            | SERCOM_USART_CTRLA_TXPO(1)                                \
+            | SERCOM_USART_CTRLA_RXPO(1)                                \
+            | SERCOM_USART_CTRLA_SAMPA(0x0)                             \
+            | SERCOM_USART_CTRLA_FORM(0)                                \
+            | (0 << SERCOM_USART_CTRLA_CMODE_Pos)                       \
+            | (0 << SERCOM_USART_CTRLA_CPOL_Pos)                        \
+            | (1 << SERCOM_USART_CTRLA_DORD_Pos),                       \
+        SERCOM_USART_CTRLB_CHSIZE(0x0)                                  \
+            | (USART_STOP_BITS_ONE << SERCOM_USART_CTRLB_SBMODE_Pos)    \
+            | (0 << SERCOM_USART_CTRLB_COLDEN_Pos)                      \
+            | (0 << SERCOM_USART_CTRLB_SFDE_Pos)                        \
+            | (0 << SERCOM_USART_CTRLB_ENC_Pos)                         \
+            | (USART_PARITY_NONE << SERCOM_USART_CTRLB_PMODE_Pos)       \
+            | (1 << SERCOM_USART_CTRLB_TXEN_Pos)                        \
+            | (1 << SERCOM_USART_CTRLB_RXEN_Pos),                       \
+        0,                                                              \
+        0,                                                              \
+        0,                                                              \
+        0,                                                              \
+    }
+
+
+enum usart_clock_mode {
+    UART_EXT_CLOCK = 0,
+    UART_INT_CLOCK = 0x1u
+};
+
+enum usart_sample_rate {
+    SAMPLE_RATE_x16 = 0x1,  //Fractional
+    SAMPLE_RATE_x8 = 0x3,   //Fractional
+};
+
+struct dmx_usart_configuration {
+    hri_sercomusart_ctrla_reg_t   ctrl_a;
+    hri_sercomusart_ctrlb_reg_t   ctrl_b;
+    hri_sercomusart_baud_reg_t    baud;
+    uint8_t                       fractional;
+    hri_sercomusart_rxpl_reg_t    rxpl;
+    hri_sercomusart_dbgctrl_reg_t debug_ctrl;
+};
+
+
 typedef void (*LXRecvCallback)(int);
 
-/*!   
+/*!
 @class LXSAMD21DMX
 @abstract
    LXSAMD21DMX is a driver for sending or receiving DMX using one of a SAM D-21's five serial peripheral interfaces (SERCOMs).
-   
+
    LXSAMD21DMX output mode continuously sends DMX once its interrupts have been enabled using startOutput().
    Use setSlot() to set the level value for a particular DMX dimmer/address/channel.
-   
+
    LXSAMD21DMX input mode continuously receives DMX once its interrupts have been enabled using startInput()
    Use getSlot() to read the level value for a particular DMX dimmer/address/channel.
-   
-   LXSAMD21DMX is used with a single instance called SAMD21DMX	.
+
+   LXSAMD21DMX is used with a single instance called SAMD21DMX  .
 */
 
 class LXSAMD21DMX  {
 
   public:
-  
-	LXSAMD21DMX  ( void );
-   ~LXSAMD21DMX ( void );
-    
+
+    LXSAMD21DMX  ( Sercom *const hw, dmx_usart_configuration conf );
+   ~LXSAMD21DMX  ( void );
+
    /*!
     * @brief starts interrupt that continuously sends DMX output
-    * @discussion Sets up baud rate, bits and parity, 
-    *             sets globals accessed in ISR, 
+    * @discussion Sets up baud rate, bits and parity,
+    *             sets globals accessed in ISR,
     *             enables transmission (TE) and tx interrupts (TIE/TCIE).
    */
    void startOutput( void );
-   
+
    /*!
     * @brief starts interrupt that continuously reads DMX data
-    * @discussion sets up baud rate, bits and parity, 
-    *             sets globals accessed in ISR, 
+    * @discussion sets up baud rate, bits and parity,
+    *             sets globals accessed in ISR,
     *             enables receive (RE) and rx interrupt (RIE)
    */
    void startInput( void );
-   
+
    /*!
     * @brief disables tx, rx and interrupts.
    */
-	void stop( void );
-	
-	/*!
-	 * @brief optional utility sets the pin used to control driver chip's
-	 *        DE (data enable) line, HIGH for output, LOW for input.     
+    void stop( void );
+
+    /*!
+     * @brief optional utility sets the pin used to control driver chip's
+     *        DE (data enable) line, HIGH for output, LOW for input.
     * @param pin to be automatically set for input/output direction
     */
    void setDirectionPin( uint8_t pin );
-	
-	/*!
-	 * @brief Sets the number of slots (aka addresses or channels) sent per DMX frame.
-	 * @discussion defaults to 512 or DMX_MAX_SLOTS and should be no less DMX_MIN_SLOTS slots.  
-	 *             The DMX standard specifies min break to break time no less than 1024 usecs.  
-	 *             At 44 usecs per slot ~= 24
-	 * @param slot the highest slot number (~24 to 512)
-	*/
-	void setMaxSlots (int slot);
-	
-	/*!
+
+    /*!
+     * @brief Sets the number of slots (aka addresses or channels) sent per DMX frame.
+     * @discussion defaults to 512 or DMX_MAX_SLOTS and should be no less DMX_MIN_SLOTS slots.
+     *             The DMX standard specifies min break to break time no less than 1024 usecs.
+     *             At 44 usecs per slot ~= 24
+     * @param slot the highest slot number (~24 to 512)
+    */
+    void setMaxSlots (int slot);
+
+    /*!
     * @brief reads the value of a slot/address/channel
-    * @discussion NOTE: Data is not double buffered.  
-    *                   So a complete single frame is not guaranteed.  
+    * @discussion NOTE: Data is not double buffered.
+    *                   So a complete single frame is not guaranteed.
     *                   The ISR continuously reads the next frame into the buffer
     * @return level (0-255)
    */
    uint8_t getSlot (int slot);
-   
-	/*!
-	 * @brief Sets the output value of a slot
-	 * @param slot number of the slot/address/channel (1-512)
-	 * @param value level (0-255)
-	*/
+
+    /*!
+     * @brief Sets the output value of a slot
+     * @param slot number of the slot/address/channel (1-512)
+     * @param value level (0-255)
+    */
    void setSlot (int slot, uint8_t value);
-   
+
    /*!
     * @brief provides direct access to data array
     * @return pointer to dmx array
    */
    uint8_t* dmxData(void);
-   
+
    /*!
     * @brief Function called when DMX frame has been read
     * @discussion Sets a pointer to a function that is called
-    *             on the break after a DMX frame has been received.  
-    *             Whatever happens in this function should be quick!  
+    *             on the break after a DMX frame has been received.
+    *             Whatever happens in this function should be quick!
     *             Best used to set a flag that is polled outside of ISR for available data.
    */
    void setDataReceivedCallback(LXRecvCallback callback);
-   
+
    /*!
     * @brief interrupt handler function
    */
    void IrqHandler();
-    
+
   private:
    /*!
     * @brief Indicates mode ISR_OUTPUT_ENABLED or ISR_INPUT_ENABLED or ISR_DISABLED
    */
-  	uint8_t  _interrupt_status;
-  	
-  	/*!
+    uint8_t  _interrupt_status;
+
+    /*!
    * @brief pin used to control direction of output driver chip
    */
-  	uint8_t _direction_pin;
-  	
-  	/*!
+    uint8_t _direction_pin;
+
+    /*!
     * @brief Array of dmx data including start code
    */
-  	uint8_t  _dmxData[DMX_MAX_SLOTS+1];
-  	
-};
+    uint8_t  _dmxData[DMX_MAX_SLOTS+1];
 
-extern LXSAMD21DMX SAMD21DMX;
+    /*!
+    * @brief Pointer to usart hardware interface. Usally set to SERCOMn
+    */
+    Sercom *const _hw;
+
+    /*!
+    * @brief USART configuration
+    */
+    dmx_usart_configuration _conf;
+
+    /*!
+    * @brief Adjust baud rate. Register values are computed
+    */
+    void setBaudRate(uint32_t baudrate);
+
+};
 
 /**************************
    Change the following lines to use other than SERCOM4.
    The following table shows the MKR1000 default uses of the SERCOMs
    and their associated pins:
-   
-   SERCOM0	Wire		11	12
-   SERCOM1  SPI		8	9	10
-   SERCOM2	ATWINC1501B SPI
-   SERCOM3	
+
+   SERCOM0  Wire        11  12
+   SERCOM1  SPI     8   9   10
+   SERCOM2  ATWINC1501B SPI
+   SERCOM3
    SERCOM4
-   SERCOM5	Serial1	13	14
-   
+   SERCOM5  Serial1 13  14
+
    Changing the SERCOM also requires changing the pin MUX.
    The following are MUX options for SERCOM3 and SERCOM4
    Mkr1000 pins
 
-	Sercom 3
-	00 S/0
-	01 S/1
-	06 SA/2
-	07 SA/3
-	08 SA/0
-	09 SA/1
-	10 SA/3
+    Sercom 3
+    00 S/0
+    01 S/1
+    06 SA/2
+    07 SA/3
+    08 SA/0
+    09 SA/1
+    10 SA/3
 
-	Sercom 4
-	04 SA/2
-	05 SA/3
+    Sercom 4
+    04 SA/2
+    05 SA/3
 
-	see datasheet pg 33 for sercom4 mux options
-	Need to be PB10 PB11, pad 2, pad 3  pad3=rx=3, pad 2=tx=1
-	which correspond to MKR10000 pins 4 & 5.
-	
-	In general, tx is either pad 0 = CRegA_TXPO 0x0 or pad 2 = CRegA_TXPO 0x1
-	see datasheet pg 481 for control register A
+    see datasheet pg 33 for sercom4 mux options
+    Need to be PB10 PB11, pad 2, pad 3  pad3=rx=3, pad 2=tx=1
+    which correspond to MKR10000 pins 4 & 5.
 
-	NOTE:  You MUST change the name of the SERCOMn_Handler() function in LXSAMD21DMX.cpp
-	       if you use a different SERCOM
+    In general, tx is either pad 0 = CRegA_TXPO 0x0 or pad 2 = CRegA_TXPO 0x1
+    see datasheet pg 481 for control register A
+
+    NOTE:  You MUST change the name of the SERCOMn_Handler() function in LXSAMD21DMX.cpp
+           if you use a different SERCOM
 */
 
-#define PIN_DMX_RX (5ul)
-#define PIN_DMX_TX (4ul)
-#define PAD_DMX_RX SERCOM_RX_PAD_3
-#define PAD_DMX_TX UART_TX_PAD_2
+#define PIN_DMX_RX DMX_RX
+#define PIN_DMX_TX DMX_TX
+#define PAD_DMX_RX PINMUX_PA04D_SERCOM0_PAD0 // TODO
+#define PAD_DMX_TX PINMUX_PA05D_SERCOM0_PAD1 // TODO
 
 // Set to PIO_SERCOM or PIO_SERCOM_ALT
-#define MUX_DMX_RX PIO_SERCOM_ALT
-#define MUX_DMX_TX PIO_SERCOM_ALT
-
-// SERCOMn is pointer to memory address where SERCOM registers are located.
-#define DMX_SERCOM SERCOM4
-// sercomN is C++ wrapper for SERCOMn (passed to UART constructor)
-#define DMX_sercom sercom4
+#define MUX_DMX_RX PINMUX_PA05D_SERCOM0_PAD1
+#define MUX_DMX_TX PINMUX_PA06D_SERCOM0_PAD2
 
 #endif // ifndef LXSAM21_DMX_H
